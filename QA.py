8a2726fae557e1e932d473827a96ce68e195a156
walidsoufiani <140754926+walidsoufiani@users.noreply.github.com>
2023-08-09 12:13:37 +0100

    Update QA.py

--- QA.py
+++ QA.py
@@ -51,45 +51,77 @@ if openai_api_key:
         chain = load_qa_chain(OpenAI(), chain_type="stuff")
     
         #manufacturer_reference={"name":''}
+
+          #creation des dictionnaires
+        composante_description = {"Name" : 'Description', "Type" : "0", "Value" : ""}
+        composante_name = {"Name" : 'Name', "Type" : "0", "Value" : ""}
+        operation_temperature = {"Name" : "Operation Temperature", "Type" : '4', "Minimum": "", "Maximum" : "" , "Units" : ""}
+        composante_weight = {"Name" : 'Weight', "Type" : 4, "Value" :"","Units": ""}
+        composant_direction={"Name" : 'Direction', "Type" : 8, "Value" : ""}
+        composant_output_current = {"Name" : 'Output current', "Type" : 4, "Value" : "","Units": ""}
     
-        query = "Give me only the name of the manufacturer reference  "
+        query = "only the first name of this model  "
         docs = docsearch.similarity_search(query)
         manufacturer_reference = chain.run(input_documents=docs, question=query)
-    
-        composante_name = {"name" : 'Name', "type" : "0", "value" : ""}
-    
-    
+        
+        
+        query = "Give me only the number of weight of this model without unit "
+        docs = docsearch.similarity_search(query)
+        weight = float(chain.run(input_documents=docs, question=query))
+        composante_weight["Value"]=weight
+        
+        query = "Give me only the unit of weight "
+        docs = docsearch.similarity_search(query)
+        weight_unit = chain.run(input_documents=docs, question=query)
+        composante_weight["Units"]=weight_unit
+        
+        query = "Give me the direction of this model, only is it 'output' or 'input' "
+        docs = docsearch.similarity_search(query)
+        direction = chain.run(input_documents=docs, question=query)
+        composant_direction["Value"]=direction
+        
+        query = "Give me only the number of the output current without unit"
+        docs = docsearch.similarity_search(query)
+        output_current = float(chain.run(input_documents=docs, question=query))
+        composant_output_current["Value"]= output_current
+        
+        query = "Give me only the symbole unit of the output current"
+        docs = docsearch.similarity_search(query)
+        output_current_unit = chain.run(input_documents=docs, question=query)
+        composant_output_current["Units"]=output_current_unit
+        
         query = "give me only the name of this model ?"
         docs = docsearch.similarity_search(query)
-        response = chain.run(input_documents=docs, question=query)
-    
-        composante_name['value'] = response
-        composante_description = {"name" : 'Description', "type" : "0", "value" : ""}
-    
+        model_name = chain.run(input_documents=docs, question=query)
+        
+        composante_name['Value'] = model_name
+        
+        
         query = "give me a description of this model"
         docs = docsearch.similarity_search(query)
-        response = chain.run(input_documents=docs, question=query)
-    
-        composante_description['value'] = response
-    
-        operation_temperature = {"name" : "Operation Temperature", "type" : '4', "minimum": "", "maximum" : "" , "unit" : ""}
-    
+        model_description = chain.run(input_documents=docs, question=query)
+        
+        composante_description['Value'] = model_description
+        
+        
+        
         query = "Give me  the integer response of the minimum operation temperature without including the unit symbole "
         docs = docsearch.similarity_search(query)
-        minimum = int(chain.run(input_documents=docs, question=query))
-    
+        minimum = float(chain.run(input_documents=docs, question=query))
+        
         query = "Give me  the integer response of the maximum operation temperature without including the unit symbole"
         docs = docsearch.similarity_search(query)
-        maximum = int(chain.run(input_documents=docs, question=query))
-    
+        maximum = float(chain.run(input_documents=docs, question=query))
+        
         query = "Give me only the symbole of the unit of the operation temperature"
         docs = docsearch.similarity_search(query)
         unit = chain.run(input_documents=docs, question=query)
-    
-        operation_temperature["maximum"] = maximum
-        operation_temperature["minimum"] = minimum
-        operation_temperature["unit"] = unit
-    
+        
+        operation_temperature["Maximum"] = maximum
+        operation_temperature["Minimum"] = minimum
+        operation_temperature["Units"] = unit
+
+
     
     
     
@@ -108,30 +140,68 @@ if openai_api_key:
             os.makedirs(json_dir)
     
     
-        c1, c2 = st.columns([1, 4])
-        c2.subheader("Parameters")
+        with st.form(key='form1'):
+            col1 , col2 = st.columns([2,1])
+            with col1:
+                placeholder1 = st.text_input("Name", value=composante_name['Value'])
+                
+                categorie = st.text_input("Manufacturer reference", value = manufacturer_reference)
+                placeholder2 = st.text_area("Description", value= composante_description["Value"])  
+
+      
+
+            with col2:
+              image = Image.open('/content/MFG_LC78_12-0.5.jpg')
+              st.image(image, caption='LC')
+              # Important
+
+            submit_button_1 = st.form_submit_button(label='Save_1')
+            if submit_button_1:
+              composante_name['Value'] = placeholder1
+              composante_description['Value'] = placeholder2
     
-        with c1:
-            placeholder1 = c2.text_input("Name", value=composante_name['value'])
-            placeholder2 = c2.text_area("Description", value= composante_description['value'])
-            placeholder3 = c2.number_input("Maximum Temperature", value=operation_temperature["maximum"])
-            placeholder4 = c2.text_input("Minimum Temperature", value=operation_temperature['minimum'])
-            placeholder5 = c2.text_input("Temperature Unit", value=operation_temperature['unit'])
-    
-        # Save Button
-        if c2.button("Save"):
-            composante_name['value'] = placeholder1
-            composante_description['value'] = placeholder2
-            operation_temperature['maximum']=placeholder3
-            operation_temperature['minimum'] = placeholder4
-            operation_temperature['unit'] = placeholder5
+        with st.form(key='form2'):
+            col1,col2,col3 = st.columns([3,2,1])
+            with col1:
+              st.markdown('<span style="color: orange; font-weight: bold;">Temperature</span>', unsafe_allow_html=True)
+              placeholder3 = st.number_input(':blue[Maximum operation Temperature]', value=operation_temperature["Maximum"])
+              placeholder4 = st.number_input(':blue[Minimum operation Temperature]', value=operation_temperature['Minimum'])
+              placeholder5 = st.text_input(':blue[Units]', value=operation_temperature['Units'])
+
+            with col3:
+              st.markdown('<span style="color: orange; font-weight: bold;">Weight</span>', unsafe_allow_html=True)
+              placeholder6 = st.number_input(':blue[Weight]', value=composante_weight["Value"])
+              placeholder7 = st.text_input(':blue[Weight Units]', value=composante_weight['Units'])
+        
+            with col2:
+              st.markdown('<span style="color: orange; font-weight: bold;">Current</span>', unsafe_allow_html=True)
+              placeholder9 = st.number_input(':blue[Output current]', value=composant_output_current["Value"])
+              placeholder10 = st.text_input(':blue[Output current Units]', value=composant_output_current['Units'])
+              placeholder8 = st.text_input(':blue[Direction]', value=composant_direction["Value"])
+
+            submit_button_2 = st.form_submit_button(label='Save_2')
+            if submit_button_2:
+              operation_temperature['Maximum']= placeholder3
+              operation_temperature['Minimum'] = placeholder4
+              operation_temperature['Units'] = placeholder5
+              composante_weight["Value"]=placeholder6
+              composante_weight['Units']=placeholder7
+              composant_direction["Value"]=placeholder8
+              composant_output_current["Value"]=placeholder9
+              composant_output_current['Units']=placeholder10
     
         # Download Button
-        if c2.button("Download JSON"):
+        if st.button("Download JSON"):
             data = {
-                "composante_name": composante_name,
-                "composante_description": composante_description,
-                "operation_temperature": operation_temperature
+                "Type":"Element/System/Component",
+                "Parameters":[
+                    composante_name,
+                    composante_description,
+                    operation_temperature,
+                    composante_weight,
+                    composant_direction,
+                    composant_output_current
+                    ]
                 }
     
             json_data = json.dumps(data, indent=4)
